bf45838738aa2b3532e2f6bc02b2fcb8531f806e
Vissakha <arpsara@ctc.fr>
2016-03-25 08:05:25 -0700

    Change gem name

--- test/lib/lambert_test.rb
+++
@@ -1,9 +0,0 @@
-require "lambert"
-
-class LambertTest < Test::Unit::TestCase
-  def test_lambert_proj
-    result = lambert_proj(45.7156058, 4.85958679999999)
-
-    assert_equal [844649.3015913883, 6514595.073166063], result
-  end
-end
